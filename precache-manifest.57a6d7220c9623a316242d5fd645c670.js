5024c4a7f97cea1ae390f0a24733bd69f5accb00
zuniversal <604688489@qq.com>
2020-01-27 18:34:25 +0800

    add 初步编写完成页面

---
+++ precache-manifest.57a6d7220c9623a316242d5fd645c670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1469e7cc57c6cf540c51877722b13c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6e1539dff74728c0833",
+    "url": "/static/css/2.5b0910b1.chunk.css"
+  },
+  {
+    "revision": "a49deb7d5481ebd63b0e",
+    "url": "/static/css/main.b70ced57.chunk.css"
+  },
+  {
+    "revision": "b6e1539dff74728c0833",
+    "url": "/static/js/2.f8421701.chunk.js"
+  },
+  {
+    "revision": "a49deb7d5481ebd63b0e",
+    "url": "/static/js/main.309a2fe6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
